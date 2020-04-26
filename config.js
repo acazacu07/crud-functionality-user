2617551f0521be2f1958633a289ec4be1ed568b0
acazacu07 <anca.cazacu07@gmail.com>
2020-04-26 13:17:03 +0100

    add user list user

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+    database: {
+        host:      'database-1.c69mclifd0fz.us-east-1.rds.amazonaws.com',     // database host
+        user:       'admin',         // your database username
+        password: 'webapp2019',         // your database password
+        port:       3306,         // default MySQL port
+        db:       'db'         // your database name
+    }
+}
+
+ 
+module.exports = config
